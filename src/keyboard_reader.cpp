85d72ae7d7e9402543ceec84d7ad09ebbed26158
Shindelhaim <sz@sem-wolf.org>
2020-07-05 07:22:10 -0700

    add class KeyboardReader

---
+++ src/keyboard_reader.cpp
@@ -0,0 +1,13 @@
+//
+// Created by tzipi on 6/30/20.
+//
+
+#include <iostream>
+#include "keyboard_reader.h"
+
+
+void keyboard_reader::initInput()
+{
+
+    std::cin >> m_data;
+}
---
+++ src/keyboard_reader.h
@@ -0,0 +1,15 @@
+#ifndef DNA_TASK_KEYBOARDREADER_H
+#define DNA_TASK_KEYBOARDREADER_H
+
+#include <string>
+#include "reader.h"
+
+class keyboard_reader : public reader
+{
+
+public:
+    void initInput();
+
+};
+
+#endif //DNA_TASK_KEYBOARDREADER_H
