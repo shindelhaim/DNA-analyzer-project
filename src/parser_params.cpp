68276f0663f9a2f76750800098f0caeff7f32c82
Shindelhaim <sz@sem-wolf.org>
2020-07-05 14:47:12 -0700

    add class ParserParams

--- src/parser_params.cpp
+++ src/parser_params.cpp
@@ -1,18 +1,26 @@
 #include <sstream>
 #include <stdio.h>
+#include <iostream>
 #include "parser_params.h"
 #include "IReader.h"
 
 
 void ParserParams::parseInput(const IReader& inputToParse, char delimiter)
 {
-    size_t i;
-    std::stringstream data(inputToParse.read());
-    std::string word;
-    for(i = 0 ;getline(data,word,delimiter);++i)
+    std::string data = inputToParse.read();
+    size_t i,pos,size = m_params.size();
+
+    for(i = 0 ;i < size && ((pos = data.find(delimiter) )!= std::string::npos);++i)
     {
-        m_params[i] = word;
+        m_params[i] = data.substr(0,pos);
+        data.erase(0,pos+1);
     }
+    while((pos = data.find(delimiter) )!= std::string::npos)
+    {
+        m_params.push_back(data.substr(0,pos));
+        data.erase(0,pos+1);
+    }
+    m_params.push_back(data.substr(0,pos));
 
 }
 
