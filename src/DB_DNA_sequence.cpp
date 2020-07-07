1ff3c1003fbf33e770b3414c4c86282acd4821b4
Shindelhaim <sz@sem-wolf.org>
2020-07-07 06:26:18 -0700

    remove static of member of class

--- src/DB_DNA_sequence.cpp
+++ src/DB_DNA_sequence.cpp
@@ -1,9 +1,6 @@
 #include "DB_DNA_sequence.h"
 #include "DNA_meta_data.h"
 
-std::map<size_t,DnaMetaData*> DataBaseDnaSequence::m_hashTableByID = std::map<size_t,DnaMetaData*>();
-std::map<std::string,size_t> DataBaseDnaSequence::m_hashTableByName = std::map<std::string,size_t>();
-
 void DataBaseDnaSequence::addNewDna(DnaMetaData *dna)
 {
     m_hashTableByID.insert(std::pair<size_t,DnaMetaData*>(dna->getId(),dna));
