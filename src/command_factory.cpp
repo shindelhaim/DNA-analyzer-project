f270b8228d49aa5675943b03330995359b103a74
Shindelhaim <sz@sem-wolf.org>
2020-07-07 02:35:41 -0700

    add class LoadCommand

--- src/command_factory.cpp
+++ src/command_factory.cpp
@@ -1,12 +1,18 @@
 #include <stdexcept>
 #include "command_factory.h"
 #include "new_command.h"
+#include "load_command.h"
 
 ICommand *CommandFactory::getCommand(const ParserParams& params)
 {
-    if(params[0] == "new")
+    std::string nameCommand = params[0];
+    if("new" == nameCommand)
     {
         return new NewCommand(params);
+    }
+    if("load" == nameCommand)
+    {
+        return new LoadCommand(params);
     }
 
     throw std::invalid_argument("COMMAND NOT FOUND");
