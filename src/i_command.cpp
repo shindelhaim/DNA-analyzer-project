461d943f5ac83f3fddd18b62ae0ca3ec21d7bad6
Shindelhaim <sz@sem-wolf.org>
2020-07-08 04:00:06 -0700

    add function getValidName

--- src/i_command.cpp
+++ src/i_command.cpp
@@ -1,6 +1,24 @@
+#include <sstream>
 #include "i_command.h"
+#include "DB_DNA_sequence.h"
+#include "DNA_meta_data.h"
 
 ICommand::ICommand(const ParserParams &params)
 {
     m_pParams = &params;
+}
+
+std::string ICommand::getValidName(const std::string &name, DataBaseDnaSequence *dataBase)
+{
+    std::string tempName = name;
+
+    while(dataBase->isNameExist(tempName))
+    {
+        DnaMetaData existDna = *(dataBase->findDnaByName(tempName));
+        std::stringstream out;
+        out << existDna.getCountCopies();
+        tempName = tempName + "_" + out.str();
+    }
+
+    return tempName;
 }
