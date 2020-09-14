324e4e2940196f1bdcb51a1a0ab898ade9707c01
Shindelhaim <sz@sem-wolf.org>
2020-09-14 18:10:19 +0300

    add class UI

--- src/View/user_interface.cpp
+++ src/View/user_interface.cpp
@@ -1,5 +1,1 @@
-//
-// Created by tzipi on 9/14/20.
-//
-
 #include "user_interface.h"
--- src/View/user_interface.h
+++ src/View/user_interface.h
@@ -1,8 +1,16 @@
-//
-// Created by tzipi on 9/14/20.
-//
-
 #ifndef SRC_USER_INTERFACE_H
 #define SRC_USER_INTERFACE_H
+
+class IReader;
+class IWriter;
+class DataBaseDnaSequence;
+
+class UI
+{
+public:
+    virtual void run(DataBaseDnaSequence* dataBaseDnaSequence, IReader* input, IWriter* output) = 0;
+
+};
+
 
 #endif //SRC_USER_INTERFACE_H
