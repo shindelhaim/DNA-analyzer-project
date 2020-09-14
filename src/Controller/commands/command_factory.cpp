557933334475067b29f0c83178840ebcf63c3be4
Shindelhaim <sz@sem-wolf.org>
2020-09-14 23:14:56 +0300

    update getCommand ana add init and release

--- src/Controller/commands/command_factory.cpp
+++ src/Controller/commands/command_factory.cpp
@@ -1,31 +1,48 @@
 #include <stdexcept>
 #include "command_factory.h"
+#include "i_command.h"
 #include "creation_commands/new_command.h"
 #include "creation_commands/load_command.h"
 #include "management_commands/save_command.h"
 #include "creation_commands/dup_command.h"
 
-ICommand *CommandFactory::getCommand(const ParserParams& params)
+std::map<std::string,ICommand*> CommandFactory::m_commandsHash;
+
+ICommand* CommandFactory::getCommand(const ParserParams& params)
 {
     std::string nameCommand = params[0];
-    if("new" == nameCommand)
+    ICommand* command;
+
+    if (nameCommand.empty())
     {
-        return new NewCommand(params);
-    }
-    if("load" == nameCommand)
-    {
-        return new LoadCommand(params);
+        throw std::invalid_argument("MISSING COMMAND");
     }
 
-    if("save" == nameCommand)
+    try
     {
-        return new SaveCommand(params);
+        command = m_commandsHash.at(nameCommand);
+        command -> initParams(params);
+        return command;
+    }
+    catch(std::out_of_range &e)
+    {
+        throw std::invalid_argument("COMMAND NOT FOUND");
     }
 
-    if("dup" == nameCommand)
-    {
-        return new DupCommand(params);
-    }
+}
 
-    throw std::invalid_argument("COMMAND NOT FOUND");
+void CommandFactory::init()
+{
+    m_commandsHash.insert(std::pair<std::string, ICommand*>("new", new NewCommand));
+    m_commandsHash.insert(std::pair<std::string, ICommand*>("load", new LoadCommand));
+    m_commandsHash.insert(std::pair<std::string, ICommand*>("dup", new DupCommand));
+    m_commandsHash.insert(std::pair<std::string, ICommand*>("save", new SaveCommand));
+}
+
+void CommandFactory::release()
+{
+    delete m_commandsHash.at("new");
+    delete m_commandsHash.at("load");
+    delete m_commandsHash.at("dup");
+    delete m_commandsHash.at("save");
 }
--- src/Controller/commands/command_factory.h
+++ src/Controller/commands/command_factory.h
@@ -2,13 +2,20 @@
 #define SRC_COMMAND_FACTORY_H
 
 #include <string>
-#include "i_command.h"
+#include <map>
 #include "../parser_params.h"
+
+class ICommand;
 
 class CommandFactory
 {
 public:
+    static void init();
+    static void release();
     static ICommand* getCommand(const ParserParams& nameCommand);
+
+private:
+    static std::map<std::string,ICommand*> m_commandsHash;
 };
 
 #endif //SRC_COMMAND_FACTORY_H
