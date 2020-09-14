7b217f38c660aa39dd623ac5cec0770e9808d715
Shindelhaim <sz@sem-wolf.org>
2020-09-14 23:18:35 +0300

    add comment that run is virtual

--- src/View/terminal.h
+++ src/View/terminal.h
@@ -10,7 +10,7 @@ class DataBaseDnaSequence;
 class Terminal : public UI
 {
 public:
-    void run(DataBaseDnaSequence* dataBaseDnaSequence, IReader* input, IWriter* output);
+    /* virtual */ void run(DataBaseDnaSequence* dataBaseDnaSequence, IReader* input, IWriter* output);
 };
 
 
