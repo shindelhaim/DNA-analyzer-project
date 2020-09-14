6c52dd5c1d547036056581e33da87b128bc51de8
Shindelhaim <sz@sem-wolf.org>
2020-09-14 23:17:17 +0300

    add call to init and release for commandFactory

--- src/Controller/system_manager.cpp
+++ src/Controller/system_manager.cpp
@@ -1,7 +1,10 @@
 #include "system_manager.h"
 #include "../View/terminal.h"
+#include "commands/command_factory.h"
 
 void SystemManager::start(UI* userInterface,IReader* input, IWriter* output)
 {
+    CommandFactory::init();
     userInterface -> run(&m_dataBaseDnaSequence,input,output);
+    CommandFactory::release();
 }
