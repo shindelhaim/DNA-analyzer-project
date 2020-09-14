16367a004f9c2708a50c2710d6c5c9625d192c24
Shindelhaim <sz@sem-wolf.org>
2020-09-14 17:53:54 +0300

    move to folder

---
+++ src/View/user_interface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tzipi on 9/14/20.
+//
+
+#include "user_interface.h"
---
+++ src/View/user_interface.h
@@ -0,0 +1,8 @@
+//
+// Created by tzipi on 9/14/20.
+//
+
+#ifndef SRC_USER_INTERFACE_H
+#define SRC_USER_INTERFACE_H
+
+#endif //SRC_USER_INTERFACE_H
