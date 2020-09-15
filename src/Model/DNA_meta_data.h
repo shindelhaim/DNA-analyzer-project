1a364d823c552a1b516f49f207f266a31bbc7f71
Shindelhaim <sz@sem-wolf.org>
2020-09-15 22:14:43 +0300

    change in getCountCopies

--- src/Model/DNA_meta_data.h
+++ src/Model/DNA_meta_data.h
@@ -20,7 +20,7 @@ public:
     const std::string& getName()const{return m_name;}
     const size_t getId()const{return m_id;}
     const DnaSequence& getDnaSequence()const{return m_dnaSequence;}
-    size_t getCountCopies() { return m_countCopies++;}
+    size_t getCountCopies() { return ++m_countCopies;}
     void setStatus(Status status){m_status = status;}
 private:
     size_t m_id;
