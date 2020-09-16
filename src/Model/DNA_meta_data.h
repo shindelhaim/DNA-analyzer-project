10341c014498947080654b6dd3988cff18d2b6ac
Shindelhaim <sz@sem-wolf.org>
2020-09-16 18:06:53 +0300

    add get status

--- src/Model/DNA_meta_data.h
+++ src/Model/DNA_meta_data.h
@@ -16,12 +16,13 @@ class DnaMetaData
 {
 public:
     DnaMetaData(const DnaSequence& dnaSequence, const std::string &name);
-    std::string getDnaDataFormat()const;
-    const std::string& getName()const{return m_name;}
-    const size_t getId()const{return m_id;}
-    const DnaSequence& getDnaSequence()const{return m_dnaSequence;}
-    size_t getCountCopies() { return ++m_countCopies;}
+    std::string getDnaDataFormat() const;
+    const std::string& getName() const{ return m_name;}
+    const size_t getId() const {return m_id;}
+    const DnaSequence& getDnaSequence() const {return m_dnaSequence;}
+    size_t getCountCopies() { return m_countCopies++;}
     void setStatus(Status status){m_status = status;}
+    Status getStatus() const { return m_status;}
 private:
     size_t m_id;
     std::string m_name;
