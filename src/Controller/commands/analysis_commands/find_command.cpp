e3edc05f5e1539e219ef381d07f14769de6bd24e
Shindelhaim <sz@sem-wolf.org>
2020-09-16 14:24:58 +0300

    add that parameter 1 can get sequence of letters

--- src/Controller/commands/analysis_commands/find_command.cpp
+++ src/Controller/commands/analysis_commands/find_command.cpp
@@ -5,6 +5,7 @@
 #include "../../../Model/DB_DNA_sequence.h"
 #include "../../parser_params.h"
 #include "../../../Model/DNA_meta_data.h"
+#include "../../../Model/DNA_sequence.h"
 
 void FindCommand::initParams(const ParserParams &parameters)
 {
@@ -20,47 +21,66 @@ void FindCommand::initParams(const ParserParams &parameters)
 
 void FindCommand::execute(DataBaseDnaSequence* dataBase,IWriter* output) const
 {
-    DnaMetaData* pDna1;
-    DnaMetaData* pDna2 = NULL;
+    const DnaSequence* pDna1 = NULL;
+    const DnaSequence* pDna2 = NULL;
+    bool isAllocate = false;
     size_t pos;
 
     if ((*m_pParams)[1][0] == '@')
     {
-        pDna1 = dataBase->findDnaByName((*m_pParams)[1].substr(1));
+        pDna1 = & ((dataBase -> findDnaByName((*m_pParams)[1].substr(1)))->getDnaSequence());
     }
-    else
+    else if((*m_pParams)[1][0] == '#')
     {
         std::istringstream in((*m_pParams)[1].substr(1));
         size_t id;
         in >> id;
-        pDna1 = dataBase->findDnaById(id);
+        pDna1 = & ((dataBase -> findDnaById(id))->getDnaSequence());
+    }
+    else
+    {
+        pDna1 = new DnaSequence((*m_pParams)[1]);
+        isAllocate = true;
     }
 
     if ((*m_pParams)[2][0] == '@')
     {
-        pDna2 = dataBase->findDnaByName((*m_pParams)[2].substr(1));
+        pDna2 = & ((dataBase -> findDnaByName((*m_pParams)[2].substr(1)))->getDnaSequence());
     }
     else if ((*m_pParams)[2][0] == '#')
     {
         std::istringstream in((*m_pParams)[2].substr(1));
         size_t id;
         in >> id;
-        pDna2 = dataBase->findDnaById(id);
+        pDna2 = & ((dataBase -> findDnaById(id))->getDnaSequence());
     }
 
     if (pDna2)
     {
-        pos = (pDna1 -> getDnaSequence()).find(pDna2->getDnaSequence());
+        pos = pDna1 -> find(*pDna2);
     }
     else
     {
-        pos = (pDna1 -> getDnaSequence()).find((*m_pParams)[2]);
+        pos = pDna1 -> find((*m_pParams)[2]);
     }
 
-
     std::stringstream out;
-    out << pos;
+
+    if ( pos >= pDna1 -> length())
+    {
+        out << "--Not found--";
+    }
+    else
+    {
+        out << pos;
+    }
+
     output -> write(out.str().c_str());
+
+    if(isAllocate)
+    {
+        delete pDna1;
+    }
 
 }
 
@@ -69,6 +89,6 @@ void FindCommand::execute(DataBaseDnaSequence* dataBase,IWriter* output) const
 bool FindCommand::is_valid()
 {
     return 3 == (*m_pParams).getSize()
-           && ((*m_pParams)[1][0] == '@' || ((*m_pParams)[1][0] == '#' && (*m_pParams)[1].substr(1).find_first_not_of("0123456789") == std::string::npos))
+           && ((*m_pParams)[1][0] != '#' || (*m_pParams)[1].substr(1).find_first_not_of("0123456789") == std::string::npos)
            && ((*m_pParams)[2][0] != '#' || (*m_pParams)[2].substr(1).find_first_not_of("0123456789") == std::string::npos);
 }
