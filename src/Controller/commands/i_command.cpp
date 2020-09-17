522ab5c0057a59447ec8f5f93c4e9f7426a4f684
Shindelhaim <sz@sem-wolf.org>
2020-09-18 00:01:58 +0300

    move func getValidName to Utils

--- src/Controller/commands/i_command.cpp
+++ src/Controller/commands/i_command.cpp
@@ -2,18 +2,3 @@
 #include "i_command.h"
 #include "../../Model/DB_DNA_sequence.h"
 #include "../../Model/DNA_meta_data.h"
-
-std::string ICommand::getValidName(const std::string &name, DataBaseDnaSequence *dataBase)
-{
-    std::string tempName = name;
-
-    while(dataBase->isNameExist(tempName))
-    {
-        DnaMetaData* existDna = dataBase->findDnaByName(tempName);
-        std::stringstream out;
-        out << existDna->getCountCopies();
-        tempName = tempName + "_" + out.str();
-    }
-
-    return tempName;
-}
