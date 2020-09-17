741227197ef658a4d0a373d9e1ef91f626b4e4fc
Shindelhaim <sz@sem-wolf.org>
2020-09-17 23:30:34 +0300

    add call func end

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include "system_manager.h"
-#include "../View/screen_writer.h"
-#include "../View/keyboard_reader.h"
-#include "../View/terminal.h"
+#include "Controller/system_manager.h"
+#include "View/screen_writer.h"
+#include "View/keyboard_reader.h"
+#include "View/terminal.h"
 
 int main() {
+
     KeyboardReader input;
     ScreenWriter output;
     Terminal terminal;
     SystemManager manager;
 
     manager.start(&terminal, &input, &output);
-
+    manager.end();
     return 0;
 }
