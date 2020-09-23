980662f524439caa399dea588e648fc70f73d943
Shindelhaim <sz@sem-wolf.org>
2020-09-23 21:52:28 +0300

    add get callback to run func

--- src/View/UI/user_interface.h
+++ src/View/UI/user_interface.h
@@ -1,14 +1,18 @@
 #ifndef SRC_USER_INTERFACE_H
 #define SRC_USER_INTERFACE_H
 
+#include "../../Controller/callback.h"
+#include "../../Controller/system_manager.h"
+
 class IReader;
 class IWriter;
 class DataBaseDnaSequence;
 
+
 class UI
 {
 public:
-    virtual void run(DataBaseDnaSequence* dataBaseDnaSequence, IReader* input, IWriter* output) = 0;
+    virtual void run(DataBaseDnaSequence* dataBaseDnaSequence,Callback<SystemManager>& callback ,IReader* input, IWriter* output) = 0;
     virtual ~UI(){}
 };
 
