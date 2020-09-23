a22e7d6e6e0510db33221157c15bdf6f9c750d84
Shindelhaim <sz@sem-wolf.org>
2020-09-23 21:51:01 +0300

    add create callback and send to run func of ui

--- src/Controller/system_manager.cpp
+++ src/Controller/system_manager.cpp
@@ -1,13 +1,23 @@
 #include "system_manager.h"
 #include "../View/UI/terminal.h"
 #include "commands/command_factory.h"
-
+#include "commands/i_command.h"
+#include "callback.h"
 void SystemManager::start(UI* userInterface,IReader* input, IWriter* output)
 {
-    userInterface -> run(&m_dataBaseDnaSequence,input,output);
+    Callback<SystemManager> callback(this, &SystemManager::executeCommand);
+    userInterface -> run(&m_dataBaseDnaSequence,callback,input,output);
 }
 
 void SystemManager::end()
 {
     CommandFactory::release();
+}
+
+void SystemManager::executeCommand(ParserParams& params, IReader *input, IWriter *output)
+{
+    const ICommand* pCommand;
+    params.parseInput(*input);
+    pCommand = CommandFactory::getCommand(params);
+    pCommand -> execute(&m_dataBaseDnaSequence, input, output);
 }
--- src/Controller/system_manager.h
+++ src/Controller/system_manager.h
@@ -8,11 +8,14 @@ class IReader;
 class IWriter;
 class UI;
 
+#include "parser_params.h"
+
 class SystemManager
 {
 public:
     void start(UI* user_interface, IReader* input, IWriter* output);
     void end();
+    void executeCommand(ParserParams& params, IReader* input, IWriter* output);
 
 private:
     DataBaseDnaSequence m_dataBaseDnaSequence;
